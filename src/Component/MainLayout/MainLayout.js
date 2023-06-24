04cc21d3d40242fa8a5107126e954691a3e3863a
Paras Arya <parasarya289@gmail.com>
2023-06-24 22:25:54 +0530

    fix: layout

--- src/Component/MainLayout/MainLayout.js
+++ src/Component/MainLayout/MainLayout.js
@@ -5,20 +5,24 @@ import "./MainLayout.css";
 export const MainLayout = ({ children, showSort }) => {
   return (
     <div className="mainLayout">
+
       <div className="mainLayout-sidebar">
         <div className="mainLayout-sidebar-content">
           <Sidebar />
         </div>
       </div>
+
       <div className="mainLayout-children">
         <p>Latest Posts</p>
         <div className="mainLayout-children-content">{children}</div>
       </div>
+
       <div className="mainLayout-sortbar">
         <div className="mainLayout-sortbar-content">
           <SortBar />
         </div>
       </div>
+      
     </div>
   );
 };
